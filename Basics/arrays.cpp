3cb2db5558d24d69ae756b7c42c2937df3db4363
Somya Kukreti <kukretisomya02@gmail.com>
2025-01-27 11:44:33 +0000

    backup

--- Basics/arrays.cpp
+++ Basics/arrays.cpp
@@ -1,39 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
-int findSecondLargest(int n, vector<int> arr)
+
+template <typename T>
+void print_vector(const vector<T> &a)
 {
-    int l = -1;
-    int sl = -1;
-    for (int i = 0; i < n; i++)
-    {
-        int num = arr[i];
-        if (num > l)
-        {
-            sl = l;
-            l = num;
-        }
-        else if (num > sl && num != l)
-        {
-            sl = num;
-        }
-    }
-    return sl;
+  for (const auto &x : a)
+  {
+    cout << x << " ";
+  }
+  cout << endl;
 }
+
+template <typename T, typename... Args>
+void print(const vector<T> &a, const Args &...args)
+{
+  print_vector(a);
+  (print_vector(args), ...);
+}
+
+void explainVector()
+{
+  vector<int> a;                           // declaration vector<data_type> name;
+  vector<int> b(5);                        // vector of size 5 a = {0, 0, 0, 0, 0}
+  vector<int> c(5, 1);                     // vector of size 5 a = {1, 1, 1, 1, 1}
+  vector<int> d = {1, 2, 3, 4, 5};         // vector of size 5 a = {1, 2, 3, 4, 5}
+  vector<int> e(d.begin() + 2, d.end()); // copy elements from first parameter to second parameter 
+  vector<int> f(e);                        // copy elements of e in f
+  print(a,b,c,d,e,f);
+
+  cout << "--------------------------\n";
+  // function to push elements in vector
+  a.push_back(2); // a = {2}
+  a.push_back(5); // a = {2, 5}
+  a.emplace_back(1); // a = {2, 5, 1}
+  print(a);
+  // q: difference between push_back and emplace_back
+  // a: push_back copies the element and emplace_back constructs the element in place
+  //    emplace_back is faster than push_back
+  // example: vector of pairs
+  // vector<pair<int, int>> v;
+  // v.push_back({1, 2}); // copy
+  // v.emplace_back(1, 2); // construct in place
+
+  cout << "--------------------------\n";
+
+  // function to pop elements from vector
+  a.pop_back(); // a = {2, 5}
+  print(a);
+
+  cout << "--------------------------\n";
+
+  // function to insert elements in vector
+  a.insert(a.begin(), 5); // a = {5, 2, 5}
+  print(a);
+  a.insert(a.begin() + 1, 2, 3); // it will insert 3, 3 at index 1 two times
+  print(a);
+  a.insert(a.begin(), d.begin(), d.end()); // insert elements from d to a
+  print(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 int main()
 {
-    int tc;
-    cin >> tc;
-    for (int i = 1; i <= tc; i++)
-    {
-        int n;
-        cin >> n;
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-        }
-        cout << findSecondLargest(n,arr) <<endl;
-    }
+  explainVector();
 
-    return 0;
+  return 0;
 }
